c222784be00aa730ce002d7064dc4bf05a81e981
1liochka1 <ila00001120@gmail.com>
2024-03-19 11:46:02 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+rpc = {'eth': 'https://rpc.ankr.com/eth',
+       'bsc': 'https://rpc.ankr.com/bsc'}
+
+claim_chain = 'eth' # bsc
+
+# гвей ниже которого будет работать скрипт
+gwei = 60
+
+# cон после транзакции
+delay = [0, 100]
